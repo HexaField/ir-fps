0ced16586ded74e3c0bda161aab64b4eb0002533
HexaField <joshfield999@gmail.com>
2024-11-19 13:38:54 +1100

    simplify

--- src/GameSystem.tsx
+++ src/GameSystem.tsx
@@ -1,7 +1,6 @@
 import { PresentationSystemGroup, defineSystem } from '@ir-engine/ecs'
-import { NetworkID, UserID, dispatchAction, useHookstate, useMutableState } from '@ir-engine/hyperflux'
-import { NetworkState } from '@ir-engine/network'
-import { NetworkPeerState } from '@ir-engine/network/src/NetworkPeerState'
+import { UserID, dispatchAction, useMutableState } from '@ir-engine/hyperflux'
+import { WorldUserState } from '@ir-engine/network/src/NetworkPeerState'
 import React, { useEffect } from 'react'
 import { PlayerActions } from './PlayerState'
 
@@ -9,26 +8,20 @@ export const GameSystem = defineSystem({
   uuid: 'hexafield.fps-game.GameSystem',
   insert: { after: PresentationSystemGroup },
   reactor: () => {
-    const networkState = useHookstate(NetworkState.worldNetworkState)
-    if (!networkState) return null
+    /** @todo this will be replaced with some lobby/game active logic */
+    const users = useMutableState(WorldUserState).keys
 
-    return <GameNetworkReactor networkID={networkState.value.id} />
+    return (
+      <>
+        {users.map((userID: UserID) => (
+          <ConnectedUserReactor key={userID} userID={userID} />
+        ))}
+      </>
+    )
   }
 })
 
-const GameNetworkReactor = (props: { networkID: NetworkID }) => {
-  const users = useMutableState(NetworkPeerState)[props.networkID].users.keys
-
-  return (
-    <>
-      {users.map((userID: UserID) => (
-        <ConnectedUserReactor key={userID} networkID={props.networkID} userID={userID} />
-      ))}
-    </>
-  )
-}
-
-const ConnectedUserReactor = (props: { networkID: NetworkID; userID: UserID }) => {
+const ConnectedUserReactor = (props: { userID: UserID }) => {
   useEffect(() => {
     dispatchAction(
       PlayerActions.playerJoined({
